fb9e5dfe0fbead3c401577e08a47e6e0e05554a7
Administrator <Administrator@Madhusudhanjami23041991-PC.mshome.net>
2021-04-23 00:47:46 +0530

    push

--- TestNG1/src/test/java/Practiceselinum/TestNG1/Ather_1.java
+++ TestNG1/src/test/java/Practiceselinum/TestNG1/Ather_1.java
@@ -31,62 +31,40 @@ public WebDriver driver;
 	@BeforeTest
 		  public void OpentheURl() {
 			
-			driver.get("https://www.facebook.com");
-			System.out.print(driver.getTitle());
-			driver.manage().window().maximize();
+		String baseUrl = "https://atherenergy.com/";
+		driver.get(baseUrl);
+		//String baseUrl1 = "https://app.atherenergy.com/product/450x/preorder";
+		//driver.get(baseUrl1);
+		driver.manage().window().maximize();
 		  }
 
 	@Test
 	       public void Flogin_page() throws InterruptedException, IOException {
-		     WebElement username = driver.findElement(By.xpath("//input[@name='email']"));
-			 username.sendKeys("madhujami91");
-			screenshots(driver, "E:\\selenium\\Screenshots\\test1.png");
-			 username.clear();
-			 System.out.print("Field is Clear");
-			 Thread.sleep(5000);
-			 WebElement password = driver.findElement(By.xpath("//*[contains(@type,'password')]"));
-			 password.sendKeys("madhujami91");
-			screenshots(driver, "E:\\selenium\\Screenshots\\test2.png");
-			 password.clear();
-			 System.out.print("\nField is Clear ");
-	}
-	@Test
-	public void Createnewaccount() throws IOException, InterruptedException {
-		 driver.findElement(By.xpath("//*[text()='Create New Account']")).click();
-		 Thread.sleep(7000);
-		screenshots(driver, "E:\\selenium\\Screenshots\\test2.png");
-		 driver.findElement(By.name("firstname")).sendKeys("Madhu");
-		 driver.findElement(By.xpath("//*[@name='lastname']")).sendKeys("Sudhan");
-		 driver.findElement(By.xpath("//*[@name='reg_email__']")).sendKeys("8056250743");
-		 WebElement day = driver.findElement(By.id("day"));
-		 WebElement month = driver.findElement(By.id("month"));
-		 WebElement year = driver.findElement(By.id("year"));
-		 SelectDropdown(day,"23");
-		 SelectDropdown(month,"May");
-		 SelectDropdown(year,"1991");
-		 driver.findElement(By.xpath("//*[text()= 'Male']")).click();
-		 screenshots(driver, "E:\\selenium\\Screenshots\\test3.png");
-		 driver.findElement(By.xpath("//*[@class='_8idr img']")).click();
-			System.out.print("\nReturned to Main page. \n");
-		screenshots(driver, "E:\\selenium\\Screenshots\\test4.png");
-	}
-	@Test
-	public void Fpassword() throws IOException, InterruptedException
-	{   
-		WebElement fpassword = driver.findElement(By.xpath("//a[text()='Forgotten password?']"));
-	    fpassword.click();
-	   screenshots(driver, "E:\\selenium\\Screenshots\\test5.png");
-		 String Madhu1 = driver.getTitle();
-		System.out.println(Madhu1);
-		 Thread.sleep(10000);
-		 driver.findElement(By.cssSelector("svg[class='eltiug27 a8c37x1j']")).click();
-		Thread.sleep(3000);
-		//call take screenshot function
-		screenshots(driver, "E:\\selenium\\Screenshots\\test6.png");
-		// driver.findElement(By.partialLinkText("Forgot")).click();
-		 System.out.println("\ntitle of page is: " + driver.getTitle());
+		driver.findElement(By.cssSelector("li[data-img='/images/450x-new/scooter-scroll/mint.png']")).click();
+	     Thread.sleep(10000);
+		System.out.print("green selected");
 		
+		
+	     driver.findElement(By.id("preorder-header-lg")).click();
+		
+	     driver.findElement(By.cssSelector("img[src='/images/mint_ather@2x.png']")).click();
+	     Thread.sleep(10000);
+	     driver.findElement(By.xpath("//*[@class='next-btn-wrapper active']")).click();
+	     Thread.sleep(5000);
+	     System.out.println("page is :  " + driver.getCurrentUrl());
+	     Thread.sleep(5000);
+	     driver.findElement(By.id("person_mail")).sendKeys("Madhujami91@gmail.com");
+        driver.findElement(By.id("person_confirm_mail")).sendKeys("Madhujami91@gmail.com");
+        driver.findElement(By.name("f_name")).sendKeys("Madhu");
+        driver.findElement(By.name("l_name")).sendKeys("Sudhan");
+        driver.findElement(By.name("contact_no")).sendKeys("8056250743");
+        driver.findElement(By.name("details[installationAddress][address_line_1]")).sendKeys("526");
+        driver.findElement(By.id("whatsappConsent")).click();
+        driver.findElement(By.cssSelector("img[src='/images/inverse-logo.svg\']")).click();
+        Thread.sleep(3000);
+        System.out.println("page is :  " + driver.getCurrentUrl());
 	}
+	
 	@AfterTest
 	public void afterMethod() throws InterruptedException {
 		Thread.sleep(3000);
