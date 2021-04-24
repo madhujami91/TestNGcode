f100a0916a988b48d6a218cd1063e0559e834251
Administrator <Administrator@Madhusudhanjami23041991-PC.mshome.net>
2021-04-24 21:55:49 +0530

    new

---
+++ FirstTestNG/src/main/java/Selinium_2/FirstTestNG/App.java
@@ -0,0 +1,13 @@
+package Selinium_2.FirstTestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ FirstTestNG/src/main/java/testNGtry/Firstcode_TetNG.java
@@ -0,0 +1,5 @@
+package testNGtry;
+
+public class Firstcode_TetNG {
+
+}
