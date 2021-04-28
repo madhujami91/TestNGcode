5b1715c6f1b39b037b1263b9eaf623773f374dc6
Administrator <Administrator@Madhusudhanjami23041991-PC.mshome.net>
2021-04-28 23:41:28 +0530

    push

--- TestNG1/src/test/java/Practiceselinum/TestNG1/Ather_1.java
+++ TestNG1/src/test/java/Practiceselinum/TestNG1/Ather_1.java
@@ -2,9 +2,11 @@ package Practiceselinum.TestNG1;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -43,10 +45,11 @@ public WebDriver driver;
 		driver.findElement(By.cssSelector("li[data-img='/images/450x-new/scooter-scroll/mint.png']")).click();
 	     Thread.sleep(10000);
 		System.out.print("green selected");
-		
-		
-	     driver.findElement(By.id("preorder-header-lg")).click();
-		
+		driver.findElement(By.id("preorder-header-lg")).click();
+		 ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(newTb.get(1));
+        System.out.println("Page title of new tab: " + driver.getTitle());
+	     
 	     driver.findElement(By.cssSelector("img[src='/images/mint_ather@2x.png']")).click();
 	     Thread.sleep(10000);
 	     driver.findElement(By.xpath("//*[@class='next-btn-wrapper active']")).click();
@@ -60,8 +63,16 @@ public WebDriver driver;
         driver.findElement(By.name("contact_no")).sendKeys("8056250743");
         driver.findElement(By.name("details[installationAddress][address_line_1]")).sendKeys("526");
         driver.findElement(By.id("whatsappConsent")).click();
+       // String clickl = Keys.chord(Keys.CONTROL,Keys.ENTER);
         driver.findElement(By.cssSelector("img[src='/images/inverse-logo.svg\']")).click();
         Thread.sleep(3000);
+        // hold all window handles in array list
+       
+        //switch to new tab
+        
+        //switch to parent window
+        driver.switchTo().window(newTb.get(0));
+        System.out.println("Page title of parent window: " + driver.getTitle());
         System.out.println("page is :  " + driver.getCurrentUrl());
 	}
 	
